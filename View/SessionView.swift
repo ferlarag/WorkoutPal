796eb4a2d413fc8237cf67f2d25e330938bcec5e
ferlarag <89590031+ferlarag@users.noreply.github.com>
2023-10-10 11:19:16 -0600

    Trivial commit

--- View/SessionView.swift
+++ View/SessionView.swift
@@ -22,13 +22,13 @@ struct SessionView: View {
                         showSubseconds: context.cadence == .live
                     )
                     .foregroundStyle(.accent)
-                    .font(.system(size: health.activity!.showDistance ? 24 : 28, design: .rounded).monospacedDigit().lowercaseSmallCaps())
+                    .font(.system(size: health.activity!.showDistance ? 24 : 32, design: .rounded).monospacedDigit().lowercaseSmallCaps())
 
                     Text(Measurement(value: health.caloriesBurned, unit: UnitEnergy.kilocalories).formatted(.measurement(width: .abbreviated, usage: .workout, numberFormatStyle: .number.precision(.fractionLength(0)))))
-                        .font(.system(size: health.activity!.showDistance ? 24 : 28, design: .rounded).monospacedDigit().lowercaseSmallCaps())
+                        .font(.system(size: health.activity!.showDistance ? 24 : 32, design: .rounded).monospacedDigit().lowercaseSmallCaps())
 
                     Text(health.heartRate.formatted(.number.precision(.fractionLength(0))) + " bpm")
-                        .font(.system(size: health.activity!.showDistance ? 24 : 28, design: .rounded).monospacedDigit().lowercaseSmallCaps())
+                        .font(.system(size: health.activity!.showDistance ? 24 : 32, design: .rounded).monospacedDigit().lowercaseSmallCaps())
 
                     if health.activity!.showDistance {
                         Text(Measurement(value: health.distance, unit: UnitLength.kilometers).formatted(.measurement(width: .abbreviated, usage: .asProvided, numberFormatStyle: .number.precision(.fractionLength(2)))))
@@ -44,7 +44,7 @@ struct SessionView: View {
 }
 
 #Preview {
-    HealthKitViewModel.shared.activity = .runningOutdoors
+    HealthKitViewModel.shared.activity = .gymnastics
     return SessionView()
         .environmentObject(HealthKitViewModel.shared)
 }
