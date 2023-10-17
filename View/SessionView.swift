f74c5384a1f933958516cd9cd874ce36a7fc04e5
ferlarag <89590031+ferlarag@users.noreply.github.com>
2023-10-17 10:21:20 -0600

    Made Metrics Text Dynamic

--- View/SessionView.swift
+++ View/SessionView.swift
@@ -15,30 +15,34 @@ struct SessionView: View {
 
     var body: some View {
         TimelineView(MetricsTimelineSchedule(from: health.builder?.startDate ?? Date(), isPaused: health.session?.state == .paused)) { context in
-            HStack(alignment: .top){
+
                 VStack(alignment: .leading){
-                    ElapsedTimeView(
-                        elapsedTime: health.builder?.elapsedTime ?? 0,
-                        showSubseconds: context.cadence == .live
-                    )
-                    .foregroundStyle(.accent)
-                    .font(.system(size: health.activity!.showDistance ? 24 : 32, design: .rounded).monospacedDigit().lowercaseSmallCaps())
+                    HStack{
+                        ElapsedTimeView(
+                            elapsedTime: health.builder?.elapsedTime ?? 0,
+                            showSubseconds: context.cadence == .live
+                        )
+                        .foregroundStyle(.accent)
+                        .font(.system(size: 500, design: .rounded).monospacedDigit().lowercaseSmallCaps())
+                                .minimumScaleFactor(0.01)
+                        Spacer()
+                    }
 
                     Text(Measurement(value: health.caloriesBurned, unit: UnitEnergy.kilocalories).formatted(.measurement(width: .abbreviated, usage: .workout, numberFormatStyle: .number.precision(.fractionLength(0)))))
-                        .font(.system(size: health.activity!.showDistance ? 24 : 32, design: .rounded).monospacedDigit().lowercaseSmallCaps())
+                        .font(.system(size: 500, design: .rounded).monospacedDigit().lowercaseSmallCaps())
+                                .minimumScaleFactor(0.01)
 
-                    Text(health.heartRate.formatted(.number.precision(.fractionLength(0))) + " bpm")
-                        .font(.system(size: health.activity!.showDistance ? 24 : 32, design: .rounded).monospacedDigit().lowercaseSmallCaps())
+                    Text(health.heartRate.formatted(.number.precision(.fractionLength(0))) + " BPM")
+                        .font(.system(size: 500, design: .rounded).monospacedDigit().lowercaseSmallCaps())
+                                .minimumScaleFactor(0.01)
 
                     if health.activity!.showDistance {
                         Text(Measurement(value: health.distance, unit: UnitLength.kilometers).formatted(.measurement(width: .abbreviated, usage: .asProvided, numberFormatStyle: .number.precision(.fractionLength(2)))))
-                            .font(.system(size: 24, design: .rounded).monospacedDigit().lowercaseSmallCaps())
+                            .font(.system(size: 500, design: .rounded).monospacedDigit().lowercaseSmallCaps())
+                                    .minimumScaleFactor(0.01)
                     }
                 }
-                .padding(.bottom)
-                Spacer()
-            }
-            .padding(.horizontal)
+                .padding(.horizontal)
         }
     }
 }
