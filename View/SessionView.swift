29c00448a361ea0615ade208cf50819aebd2d84e
ferlarag <89590031+ferlarag@users.noreply.github.com>
2023-10-17 11:48:38 -0600

    Metrics font size optimization

--- View/SessionView.swift
+++ View/SessionView.swift
@@ -13,42 +13,93 @@ struct SessionView: View {
     @State private var sessionNav: SessionSecondaryNavigation = .metrics
     @State private var showTotalTime = true
 
+    var commonFont: Font {
+        if health.activity!.activityType.supportsDistance {
+            Font.system(size: 24, design: .rounded)
+                .monospacedDigit()
+                .lowercaseSmallCaps()
+        } else {
+            Font.system(size: 32, design: .rounded)
+                .monospacedDigit()
+                .lowercaseSmallCaps()
+        }
+    }
+
     var body: some View {
         TimelineView(MetricsTimelineSchedule(from: health.builder?.startDate ?? Date(), isPaused: health.session?.state == .paused)) { context in
-
+            HStack{
                 VStack(alignment: .leading){
-                    HStack{
-                        ElapsedTimeView(
-                            elapsedTime: health.builder?.elapsedTime ?? 0,
-                            showSubseconds: context.cadence == .live
-                        )
-                        .foregroundStyle(.accent)
-                        .font(.system(size: 500, design: .rounded).monospacedDigit().lowercaseSmallCaps())
-                                .minimumScaleFactor(0.01)
-                        Spacer()
-                    }
+                    ElapsedTimeView(
+                        elapsedTime: health.builder?.elapsedTime ?? 0,
+                        showSubseconds: context.cadence == .live
+                    )
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.accent)
+                    .font(commonFont)
 
-                    Text(Measurement(value: health.caloriesBurned, unit: UnitEnergy.kilocalories).formatted(.measurement(width: .abbreviated, usage: .workout, numberFormatStyle: .number.precision(.fractionLength(0)))))
-                        .font(.system(size: 500, design: .rounded).monospacedDigit().lowercaseSmallCaps())
-                                .minimumScaleFactor(0.01)
 
-                    Text(health.heartRate.formatted(.number.precision(.fractionLength(0))) + " BPM")
-                        .font(.system(size: 500, design: .rounded).monospacedDigit().lowercaseSmallCaps())
-                                .minimumScaleFactor(0.01)
+                    Text(Measurement(value: health.caloriesBurned, 
+                                     unit: UnitEnergy.kilocalories)
+                        .formatted(.measurement(width: .abbreviated,
+                                                usage: .workout,
+                                                numberFormatStyle: .number.precision(.fractionLength(0)))))
+                    .font(commonFont)
+
+                    Text(health.heartRate.formatted(.number.precision(.fractionLength(0))) + " Bpm")
+                        .font(commonFont)
 
                     if health.activity!.showDistance {
                         Text(Measurement(value: health.distance, unit: UnitLength.kilometers).formatted(.measurement(width: .abbreviated, usage: .asProvided, numberFormatStyle: .number.precision(.fractionLength(2)))))
-                            .font(.system(size: 500, design: .rounded).monospacedDigit().lowercaseSmallCaps())
-                                    .minimumScaleFactor(0.01)
+                            .font(commonFont)
                     }
                 }
-                .padding(.horizontal)
+                .padding(.bottom)
+                Spacer()
+            }
         }
     }
 }
 
 #Preview {
-    HealthKitViewModel.shared.activity = .gymnastics
-    return SessionView()
+    HealthKitViewModel.shared.activity = .functionalStrengthTraining
+    return NavigationStack{
+        SessionView()
+            .toolbar{
+                ToolbarItem(placement: .topBarLeading) {
+                    Button{
+
+                    } label: {
+                        Label("Text", systemImage: "timer")
+                    }
+                }
+
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button{
+
+                    } label: {
+                        Label("Toggle Pause", systemImage: "pause")
+                    }
+                    .foregroundStyle(.white)
+                }
+
+                ToolbarItem(placement: .bottomBar) {
+                    Button{
+
+                    } label: {
+                        Label("Activity", systemImage: "xmark")
+                    }
+                    .foregroundStyle(.accent)
+                }
+
+                ToolbarItem(placement: .bottomBar) {
+                    Button{
+
+                    } label: {
+                        Label("Finish", systemImage: "checkmark")
+                    }
+                    .foregroundStyle(.accent)
+                }
+            }
+    }
         .environmentObject(HealthKitViewModel.shared)
 }
